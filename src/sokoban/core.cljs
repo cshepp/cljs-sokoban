ed1bff09b4861fc55b91f19f5a83967b6959f1c1
cshepp <me@codyshepp.com>
2015-04-04 11:58:59 -0400

    Added core.match dependency

--- src/sokoban/core.cljs
+++ src/sokoban/core.cljs
@@ -1,16 +1,20 @@
 (ns sokoban.core
     (:require [cljs.core.async :as async :refer [<! >! chan]]
+              [clojure.core.match :refer [match]]
               [sokoban.render :as r]
               [sokoban.map :as m])
     (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
 (enable-console-print!)
 
-(def serialized-state (atom [[:w :w :w :w :w :w :w]
-                             [:w :n :n :n :n :n :w]
-                             [:w :n :p :b :g :n :w]
-                             [:w :n :n :n :n :n :w]
-                             [:w :w :w :w :w :w :w]]))
+(def state (atom (m/deserialize-state [[:w :w :w :w :w :w :w]
+                                       [:w :n :n :n :n :n :w]
+                                       [:w :n :p :b :g :n :w]
+                                       [:w :n :n :n :n :n :w]
+                                       [:w :w :w :w :w :w :w]])))
+
+(defn get-player [state]
+  )
 
 (defn on-input [ch]
   (go-loop []
@@ -25,4 +29,4 @@
     (on-input input-chan)
     (r/init input-chan)
     (go
-        (>! render-chan @deserialized-state)))
+        (>! render-chan @state)))
